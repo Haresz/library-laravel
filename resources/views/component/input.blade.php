2a95800b242037519c8ecfde63c95c394bf666c0
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-04 14:38:29 +0700

    feat: some component

--- resources/views/component/input.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="mb-3">
-    <label class="my-2" for="{{$name}}">{{$name}}</label>
-    <input type="text" name="{{$name}}" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
-</div>
