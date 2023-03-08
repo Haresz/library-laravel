50f9a0539fb7b205fa1ea6723869d1053df94f22
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-08 21:25:40 +0700

    feat: auth layout

---
+++ resources/views/layouts/auth.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-100">
+    <head>
+        @includeIf('partials.head.meta')
+        @includeIf('partials.head.title')
+        @includeIf('partials.head.auth.css')
+    </head>
+    <body class="h-100" >
+        @hasSection ('page-content')
+            @yield('page-content')
+        @endif
+    </body>
+</html>
