89dfc70f39a1665d8626cc63938bc4cb729f5e08
haris <89023322+Haresz@users.noreply.github.com>
2023-03-13 09:08:28 +0700

    fix ui search admin

--- resources/views/partials/form/admin/search.blade.php
+++ resources/views/partials/form/admin/search.blade.php
@@ -1,9 +1,9 @@
 <nav class="d-flex justify-content-between p-5 text-light w-100 border border-bottom-5" style="background-color: white; font-family: 'Inter', sans-serif; top: 0;  right: 0;">
     <p class="h4 text-dark">Daftar Buku</p>
     <div style="width: 600px;">
-        <div class="d-flex">
+        <div class="d-flex d-flex justify-content-between">
             <form action="{{ route('filter') }}" method="GET">
-                <input class="form-control me-2 rounded-pill text-dark" style="background: rgba(255, 255, 255, 0.15);" type="text" name="filter" placeholder="Cari judul buku" aria-label="Cari judul buku" aria-describedby="basic-addon2" value="{{ old('filter', request()->filter) }}">
+                <input class="form-control me-2 rounded-pill text-dark" style="background: rgba(255, 255, 255, 0.15); width: 350px;" type="text" name="filter" placeholder="Cari judul buku" aria-label="Cari judul buku" aria-describedby="basic-addon2" value="{{ old('filter', request()->filter) }}">
             </form>
             <div class="dropdown">
                 <a href="#" class="d-flex align-items-center link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
